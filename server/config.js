79d95677f0794e5fac9ebf3420cd2687ee1b54b6
anyu <ayu3500@gmail.com>
2017-05-03 11:06:51 -0700

    Add node to package.json, set MongoLab url correctly

--- server/config.js
+++ server/config.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
-var database = process.env_MONGOLAB_URI  || 'mongodb://localhost/yujinerator';
+var database = 'mongodb://heroku_4d6s6cbg:d9154prsu0kndq07hn8nhkhmkl@ds129641.mlab.com:29641/heroku_4d6s6cbg' || 'mongodb://localhost/yujinerator';
 mongoose.connect(database);
 
 var db = mongoose.connection;
