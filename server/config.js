6b4c4253bfcc6566b75109d1060fae323e23316d
anyu <ayu3500@gmail.com>
2017-05-03 10:49:20 -0700

    Add db path to MONGOLAB_URI

--- server/config.js
+++ server/config.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
-var database = process.env_MONGO_URL || 'mongodb://localhost/yujinerator';
+var database = process.env_MONGOLAB_URI  || 'mongodb://localhost/yujinerator';
 mongoose.connect(database);
 
 var db = mongoose.connection;
